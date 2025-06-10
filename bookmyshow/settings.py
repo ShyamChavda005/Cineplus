3c63f8998cbcde0763367ef63dafc6057fd4a120
ShyamChavda005 <shyamchavda005@gmail.com>
2025-06-10 16:06:21 +0530

    first commit

--- bookmyshow/settings.py
+++ bookmyshow/settings.py
@@ -138,5 +138,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'Your Email'
-EMAIL_HOST_PASSWORD = 'Gmail App Password'
+EMAIL_HOST_USER = 'teamhealthcarehospital@gmail.com'
+EMAIL_HOST_PASSWORD = 'pvvmidjrwmtawrpy'
