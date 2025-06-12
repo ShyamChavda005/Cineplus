f1a40055b1776bc6f3b2b31cc9b5d39d8fc6c1f3
ShyamChavda005 <shyamchavda005@gmail.com>
2025-06-12 15:01:45 +0530

    first commit

--- home/views.py
+++ home/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render, redirect
 # from django.contrib.auth.models import User
 from movie.models import *
+from datetime import *
 
 # Create your views here.
 
 def index(request) :    
-    return render(request,"index.html")
+    today = date.today()
+    today_shows = Theater.objects.filter(datetime__date=today).select_related("movie")
+
+    grouped_shows = {}
+    for show in today_shows:
+        movie = show.movie
+        if movie not in grouped_shows:
+            grouped_shows[movie] = []
+        grouped_shows[movie].append(show)
+    
+    return render(request,"index.html",{'grouped_shows': grouped_shows})
 
 def about(request) :
     return render(request,"about.html")
